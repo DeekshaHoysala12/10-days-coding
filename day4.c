6bb3846f2f6cbe3b8dbdc8b7bfc54958e984ea8c
DeekshaHoysala12 <84765450+DeekshaHoysala12@users.noreply.github.com>
2021-11-21 17:03:15 +0530

    day4

---
+++ day4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int a[100],key,n,i,index;
+    printf("Enter the number of elements in an array\n");
+    scanf("%d",&n);
+    printf("Enter array elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("The array elements are\n");
+    for(i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+    printf("Enter the key element to be found\n");
+    scanf("%d",&key);
+    for(i=0;i<n;i++){
+        if(a[i]==key){
+            index=i;
+            break;
+           
+        }
+        
+    }
+    if(index==i){
+        printf("The entered element %d is found in %d position",key,i+1);
+    }
+    else{
+            printf("The entered element not found");
+        }
+    
+    
+    return 0;
+}
