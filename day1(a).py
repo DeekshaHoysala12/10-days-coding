0b667d55aaea3b19ce1964c5a345ee20341b2f05
DeekshaHoysala12 <84765450+DeekshaHoysala12@users.noreply.github.com>
2021-11-28 11:01:50 +0530

    day1

--- day1(a).py
+++ day1(a).py
@@ -1,9 +1,9 @@
-n=input("number")
-m=input("digit")
+n=input("Enter the Number: ")
+m=input("Enter the Digit: ")
 count=0
 
 for i in n:
     if i!='\0':
         if i==m:
             count=count+1
-print(count)
+print("Digit Frequency of the given Digit is ",count,".")
